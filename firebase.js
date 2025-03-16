a2ab536ba5e6ba85de115642793c380cc0718119
KIMM212 <abbaskim25@gmail.com>
2025-03-16 20:37:21 +0300

    firebase.js

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC8mV4rBF0jEPEEvQNXN32bHw84Z47NqGE",
+  authDomain: "techcrackersweb.firebaseapp.com",
+  projectId: "techcrackersweb",
+  storageBucket: "techcrackersweb.firebasestorage.app",
+  messagingSenderId: "283662279410",
+  appId: "1:283662279410:web:f9e49de81c7d72d511266b",
+  measurementId: "G-7YCBTKM7JH"
+};
